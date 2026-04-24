7d5bc22051b98edf28e5eec37ce9eaa40d402f7d
Ing. Matěj Klíma <klima@beanmail.cz>
2026-04-24 09:14:32 +0200

    První funkce v Pythonu

---
+++ uvod/funkce.py
@@ -0,0 +1,8 @@
+def umocneni():
+    cisloStr = input("Zadejte číslo")
+    cislo1 = int(cisloStr)
+    print("číslo je: ", cislo1)
+    print("číslo na druhou je: ", cislo1 * cislo1)
+    print("číslo na třetí je: ", cislo1 ** 3)
+
+umocneni()
