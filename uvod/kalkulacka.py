9b1587079859366ec294c0c45f602316e9e0419f
Ing. Matěj Klíma <klima@beanmail.cz>
2026-04-28 12:00:07 +0200

    Přidání dalších operací do kalkulačky

--- uvod/kalkulacka.py
+++ uvod/kalkulacka.py
@@ -10,7 +10,14 @@ elif (operace == "-"):
     vysledek = prvniCislo - druheCislo
 elif (operace == "*"):
     vysledek = prvniCislo * druheCislo
+elif (operace == "u"):
+    vysledek = prvniCislo ** druheCislo
+elif (operace == "o"):
+    vysledek = prvniCislo ** (1 / druheCislo)
 else:
     vysledek = prvniCislo / druheCislo
 # zobraz vysledek
 print("Výsledek je", vysledek)
+
+# odmocneni : o
+# umocneni : u    ** 
