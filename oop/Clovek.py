43c97534b6e2b327ce38de3425767a3d92077450
Ing. Matěj Klíma <klima@beanmail.cz>
2026-06-12 08:47:56 +0200

    Třída Clovek a kolekce zaku tridy 3E

--- oop/Clovek.py
+++ oop/Clovek.py
@@ -1,13 +1,27 @@
 class Clovek:
+    
     def __init__(self, jmeno="", prijmeni="", pohlavi="", vek=-1):
         self.jmeno = jmeno
         self.prijmeni = prijmeni
         self.pohlavi = pohlavi
         self.vek = vek
+    
+    def __str__(self):
+        return f"Clovek => Jméno: {self.jmeno}, Příjmení: {self.prijmeni}, Pohlaví: {self.pohlavi}, Věk: {self.vek}"
 
-honza = Clovek("Honza", "Novák", "muž", 20)
-petr = Clovek()
-petr.jmeno = "Petr"
-petr.prijmeni = "Dostál"
-print(honza.jmeno)
-print(petr.jmeno)
+    def jeMuz(self):
+        if (self.pohlavi == "muz" or self.pohlavi == "muž"):
+            return True
+        else:
+            return False
+
+zaci = []        
+zaci.append(Clovek("David", "Reschke", "muž", 19))
+zaci.append(Clovek("Petr", "Bui", "muž", 19))
+zaci.append(Clovek("Natalie", "Mikšovská", "žena", 19))
+zaci.append(Clovek("Armin", "Lyavinets", "muž", 18))
+zaci.append(Clovek("Nicholas", "Ras", "muž", 18))
+zaci.append(Clovek("Samuel", "El Bakri", "muž", 18))
+
+for zak in zaci:
+    print(zak)
